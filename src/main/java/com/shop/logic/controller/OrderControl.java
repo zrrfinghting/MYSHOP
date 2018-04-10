d97b82b13a64acb9408e53833ff2644568d5f86e
zrrfinghting <799694563@qq.com>
2018-04-10 23:31:54 +0800

    增加了订单的查询

--- src/main/java/com/shop/logic/controller/OrderControl.java
+++ src/main/java/com/shop/logic/controller/OrderControl.java
@@ -1,23 +1,26 @@
 package com.shop.logic.controller;
 
 import com.shop.basic.BasicFilter;
-import com.shop.logic.dao.LogDao;
-import com.shop.logic.dao.OrderDao;
-import com.shop.logic.dao.OrderGoodsDao;
-import com.shop.logic.dao.ShopCartDao;
-import com.shop.logic.domain.Order;
-import com.shop.logic.domain.OrderGoods;
-import com.shop.logic.domain.ShopCart;
+import com.shop.logic.dao.*;
+import com.shop.logic.domain.*;
 import com.shop.logic.util.JsonUtil;
+import com.shop.logic.util.TableUtil;
 import io.swagger.annotations.ApiOperation;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.filter.OrderedRequestContextFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 订单对外服务类
@@ -35,6 +38,10 @@ public class OrderControl {
     private ShopCartDao shopCartDao;
     @Autowired
     private OrderGoodsDao orderGoodsDao;
+    @Autowired
+    private AddrDao addrDao;
+    @Autowired
+    private GoodsDao goodsDao;
     @Autowired
     private LogDao logDao;
 
@@ -116,4 +123,91 @@ public class OrderControl {
         }
     }
 
+    /**
+     * 返回给用户的信息有  订单地址信息、商品信息、下单时间、订单编号、商品总额
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "getById", method = RequestMethod.GET)
+    @ApiOperation(value = "根据Id获取订单信息", notes = "根据Id获取订单信息")
+    public String getById(@RequestParam String id){
+        try {
+            Order order = orderDao.findById(id);
+            Address address = addrDao.findById(order.getAddressId());
+            List<OrderGoods> ogs = orderGoodsDao.findAllByOrderId(id);
+            LinkedList<Object> goodss = new LinkedList<>();
+            double totalPrice = 0;
+            int totalNumber = 0;
+            for (OrderGoods og:ogs){
+                Goods goods = goodsDao.findById(og.getGoodsId());
+                JSONObject goodsJson = JSONObject.fromObject(goods);
+                goodsJson.put("number",og.getNumber());
+                goodss.add(goodsJson);
+                totalPrice += og.getNumber()*goods.getPrice();
+                totalNumber +=og.getNumber();
+            }
+
+            JSONObject orderJson = JSONObject.fromObject(order);
+            JSONObject addressJson = JSONObject.fromObject(address);
+            JSONArray goodsArray = JSONArray.fromObject(goodss);
+            JSONObject resultJson = new JSONObject();
+            resultJson.putAll(orderJson);
+            resultJson.putAll(addressJson);
+            resultJson.put("list",goodsArray);
+            resultJson.put("totalPrice",totalPrice);
+            resultJson.put("totalNumber",totalNumber);
+            return resultJson.toString();
+        }catch (Exception e){
+            e.printStackTrace();
+            return JsonUtil.returnStr(JsonUtil.FAIL,"获取订单失败");
+        }
+    }
+
+    /**
+     * 只给返回每条订单的  商品名称，商品数量，商品总价
+     *
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping(value = "getByPage", method = RequestMethod.POST)
+    @ApiOperation(value = "获取用户所有订单信息", notes = "获取用户所有订单信息")
+    public String getByPage(
+            @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+            @RequestParam(value = "pageSize", defaultValue = "15") Integer pageSize){
+        try {
+            Pageable pageable = new PageRequest(pageNum-1,pageSize,new Sort(Sort.Direction.DESC,"createTime"));
+            Page<Order> orders = orderDao.findAllByUserCode(BasicFilter.user_id,pageable);
+            LinkedList<String> orderIds = new LinkedList<>();
+            for (Order order:orders){
+                orderIds.add(order.getId());
+            }
+            LinkedList<Object> list = new LinkedList<>();
+            for (String id:orderIds){
+                List<OrderGoods> ogs = orderGoodsDao.findAllByOrderId(id);
+                LinkedList<Goods> goodss = new LinkedList<>();
+                int totalNumber = 0;
+                for (OrderGoods og:ogs){
+                    Goods goods = goodsDao.findById(og.getGoodsId());
+                    goodss.add(goods);
+                    totalNumber += og.getNumber();
+                }
+                JSONArray array = JSONArray.fromObject(goodss);
+                JSONObject json = JSONObject.fromObject(array);
+                json.put("orderid",id);
+                json.put("totalNumber",totalNumber);
+                list.add(json);
+            }
+
+            long total = orderDao.countAllByUserCode(BasicFilter.user_id);
+            long totalPage = total/pageSize==0 ? total/pageSize:total/pageSize+1;
+            if (totalPage==0){
+                totalPage=1;
+            }
+            return TableUtil.createTableDate(list,total,pageNum,totalPage,pageSize);
+        }catch (Exception e){
+            e.printStackTrace();
+            return JsonUtil.returnStr(JsonUtil.FAIL,"获取失败");
+        }
+    }
 }
--- src/main/java/com/shop/logic/dao/OrderGoodsDao.java
+++ src/main/java/com/shop/logic/dao/OrderGoodsDao.java
@@ -5,6 +5,9 @@ package com.shop.logic.dao;/**
 import com.shop.logic.domain.OrderGoods;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 订单商品中间表
  *
@@ -12,5 +15,12 @@ import org.springframework.data.repository.CrudRepository;
  * @creat 2018/4/10
  */
 public interface OrderGoodsDao extends CrudRepository<OrderGoods,Integer>{
+
+    /**
+     * 获取订单商品中间表对象  从而得到订单对应的商品
+     * @param orderId
+     * @return
+     */
+    public List<OrderGoods> findAllByOrderId(String orderId);
 
 }
